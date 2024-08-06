b362ecd1c7667b0f9c6aa582672f903a276b4593
Zignuts Parth Gupta <parthg@zignuts.com>
2024-08-06 18:03:08 +0530

    reset password functionality working correctly

--- apis/api.py
+++ apis/api.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from items.views import register_user,login_user,all_items,single_item,create_item,update_item,delete_item,change_password
+from items.views import register_user,login_user,all_items,single_item,create_item,update_item,delete_item,change_password,request_password_reset,reset_password
 
 urlpatterns = [
    path('register-user',register_user),
@@ -10,5 +10,7 @@ urlpatterns = [
    path('create-item',create_item),
    path('update-item/<int:item_id>',update_item),
    path('delete-item/<int:item_id>',delete_item),
-   path('change-password',change_password)
+   path('change-password',change_password),
+   path('request-password-reset', request_password_reset,name='request-password-reset'),
+   path('reset-password/<uidb64>/<token>',reset_password,name='reset-password'),
 ]
--- items/views.py
+++ items/views.py
@@ -14,6 +14,14 @@ from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 from django.utils import timezone
 from rest_framework.pagination import PageNumberPagination
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_str
+from rest_framework.views import APIView
+from django.urls import reverse
+
+token_generator = PasswordResetTokenGenerator()
+
 @api_view(['POST'])
 @permission_classes([])
 def register_user(request):
@@ -160,3 +168,57 @@ def change_password(request):
         return Response({'message': 'Password changed successfully. A notification email has been sent.'}, status=status.HTTP_200_OK)
     except Exception as e:
         return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+@permission_classes([])
+def request_password_reset(request):
+    email = request.data.get('email')
+    try:
+        user       = User.objects.get(email=email)
+        token      = token_generator.make_token(user)
+        uid        = urlsafe_base64_encode(force_bytes(user.pk))
+        reset_link = request.build_absolute_uri(reverse('reset-password', args=[uid, token]))
+
+        subject = 'Password Reset Request'
+        html_message = render_to_string('password_reset_email.html', {
+            'user': user,
+            'reset_link': reset_link,
+        })
+        plain_message = strip_tags(html_message)
+        send_mail(
+            subject,
+            plain_message,
+            settings.EMAIL_HOST_USER,
+            [user.email],
+            fail_silently=False,
+            html_message=html_message,
+        )
+        return Response({'message': 'Password reset link sent successfully.'}, status=status.HTTP_200_OK)
+    except User.DoesNotExist:
+        return Response({'error': 'User with this email does not exist.'}, status=status.HTTP_404_NOT_FOUND)
+    except Exception as e:
+        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
+@api_view(['POST'])
+@permission_classes([])
+def reset_password(request, uidb64, token):
+    try:
+        uid  = force_str(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+
+        if token_generator.check_token(user, token):
+            new_password = request.data.get('new_password')
+            if not new_password:
+                return Response({'error': 'New password cannot be empty.'}, status=status.HTTP_400_BAD_REQUEST)
+
+            user.set_password(new_password)
+            user.save()
+            
+
+            return Response({'message': 'Password reset successfully.'}, status=status.HTTP_200_OK)
+        else:
+            return Response({'error': 'Invalid token.'}, status=status.HTTP_400_BAD_REQUEST)
+    except User.DoesNotExist:
+        return Response({'error': 'User does not exist.'}, status=status.HTTP_404_NOT_FOUND)
+    except Exception as e:
+        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
